ccfe42def026a6771d2a77b1c104a2f873509e99
AkemiHomura-maow <34213712+AkemiHomura-maow@users.noreply.github.com>
2023-11-10 02:29:35 +0800

    feat(pricefetcher): storing source tokens and connectors in storage to reduce calldata size of fetchPrices

--- contracts/PriceFetcher.sol
+++ contracts/PriceFetcher.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.13;
 
@@ -14,6 +14,35 @@ contract PriceFetcher {
     IVeloOracle public oracle;
     address public owner;
     mapping(address => bool) public callers;
+    address[] public source_tokens;
+    address[] public connectors;
+
+    function add_tokens(bool is_connector, address[] calldata _tokens) public{
+        _only_owner();
+        address[] storage arr = is_connector ? connectors : source_tokens;
+        for (uint i = 0; i < _tokens.length; i++) {
+            arr.push(_tokens[i]);
+        }
+    }
+
+    function remove_tokens(bool is_connector, uint256[] calldata _indices) public{
+        _only_owner();
+        address[] storage arr = is_connector ? connectors : source_tokens;
+        for (uint i = 0; i < _indices.length; i++) {
+            arr[_indices[i]] = arr[arr.length - 1];
+            arr.pop();
+        }
+    }
+
+    function _construct_oracle_args(uint256 _src_len, uint256 _src_offset) internal view returns (IERC20Metadata[] memory oracle_args){
+        oracle_args = new IERC20Metadata[]( _src_len + connectors.length );
+        for (uint i = _src_offset; i < _src_offset + _src_len; i++) {
+            oracle_args[i - _src_offset] = IERC20Metadata(source_tokens[i]);
+        }
+        for (uint i = _src_len; i < oracle_args.length; i++) {
+            oracle_args[i] = IERC20Metadata(connectors[i - _src_len]);
+        }
+    }
 
     /// @notice Emitted when a price for a token is fetched.
     /// @param token The address of the token.
@@ -27,11 +56,15 @@ contract PriceFetcher {
         owner = msg.sender;
     }
 
+    function _only_owner() internal view{
+        require(msg.sender == owner);
+    }
+
     /// @notice Adds a caller to the callers set.
     /// @dev Can only be called by the owner.
     /// @param _caller The address to be added.
     function add_caller(address _caller) public {
-        require(msg.sender == owner);
+        _only_owner();
         callers[_caller] = true;
     }
 
@@ -39,7 +72,7 @@ contract PriceFetcher {
     /// @dev Can only be called by the owner.
     /// @param _caller The address to be removed.
     function remove_caller(address _caller) public {
-        require(msg.sender == owner);
+        _only_owner();
         callers[_caller] = false;
     }
 
@@ -47,7 +80,7 @@ contract PriceFetcher {
     /// @dev Can only be called by the current owner.
     /// @param _owner The address of the new owner.
     function change_owner(address _owner) public {
-        require(msg.sender == owner);
+        _only_owner();
         owner = _owner;
     }
 
@@ -55,21 +88,22 @@ contract PriceFetcher {
     /// @dev Can only be called by the current owner.
     /// @param _oracle The address of the new VeloOracle.
     function change_oracle(IVeloOracle _oracle) public {
-        require(msg.sender == owner);
+        _only_owner();
         oracle = _oracle;
     }
 
     /// @notice Fetches prices for a list of tokens.
     /// @dev Can only be called by the owner. Emits a PriceFetched event for each token.
-    /// @param src_len The number of tokens to fetch prices for.
-    /// @param connectors The list of tokens to fetch prices for.
-    function fetchPrices(uint8 src_len, IERC20Metadata[] memory connectors) public {
+    /// @param _src_len The number of source tokens to fetch prices for.
+    /// @param _src_offset The number of source tokens to skip. 
+    function fetchPrices(uint _src_len, uint _src_offset) public {
         require(msg.sender == owner || callers[msg.sender]);
 
-        uint256[] memory prices = oracle.getManyRatesWithConnectors(src_len, connectors);
+        uint256[] memory prices = oracle.getManyRatesWithConnectors(uint8(_src_len), _construct_oracle_args(_src_len, _src_offset));
 
-        for (uint8 i = 0; i < src_len; i++) {
-            emit PriceFetched(address(connectors[i]), prices[i]);
+        for (uint i = _src_offset; i < _src_offset + _src_len; i++) {
+            emit PriceFetched(source_tokens[i], prices[i - _src_offset]);
         }
     }
+
 }
